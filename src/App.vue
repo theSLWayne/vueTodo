45be9f950ee2903b91e1a3281014561724528eac
theSLWayne <hiranwayne@yahoo.com>
2019-05-05 23:57:49 +0530

    Created adding new todo items

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header /> 
-    <AddTodo />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos='todos' v-on:del-todo="deleteToDo" />
   </div>
 </template>
@@ -42,6 +42,9 @@ export default {
   methods: {
     deleteToDo(id){
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
