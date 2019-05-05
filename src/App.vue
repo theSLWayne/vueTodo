a6687dca0455ee4c467d4abaaa6cf2bafe9279e0
theSLWayne <hiranwayne@yahoo.com>
2019-05-05 16:47:23 +0530

    First Commi

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos='todos' v-on:del-todo="deleteToDo" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo Three',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteToDo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        <h1>Todolist</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "header"
+}
+</script>
+
+<style scoped>
+
+</style>
