fb6094ff94810f02519361fb0d9dedda9df052d5
theSLWayne <hiranwayne@yahoo.com>
2019-05-06 23:10:36 +0530

    get todo items via an API

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'app',
@@ -20,23 +21,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
@@ -46,6 +31,11 @@ export default {
     addTodo(newTodo){
       this.todos = [...this.todos, newTodo];
     }
+  },
+  created(){
+      axios.get('http://jsonplaceholder.typicode.com/todos')
+        .then(res => this.todos = res.data)
+        .catch();
   }
 }
 </script>
